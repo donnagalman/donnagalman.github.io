b2e8c08994293e1f91fcf3a484b348eb74cd6b08
Donna <donna@connectica.no>
2018-06-09 13:42:04 +0700

    navbar

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,12 @@
-$(".sliding-link").click(function(e) {
-    e.preventDefault();
-    var aid = $(this).attr("href");
-    $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
 });
